4fe90e567d6f12e861395d89f41b3ee3bf5b85fd
SpaceTurtle-git <spaceturte@gmail.com>
2026-04-30 05:28:14 +0530

    test1

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ class Config:
     GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "")
     # Store DB in project root instance/ folder
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'ayurveda.db')}"
+    # config.py
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///instance/ayurveda.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
