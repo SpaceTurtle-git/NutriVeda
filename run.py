bb330e1b6148bc1e373d26eec741325181cb8c9e
SpaceTurtle-git <spaceturte@gmail.com>
2026-04-30 05:35:47 +0530

    test2

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    os.makedirs("instance", exist_ok=True)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
